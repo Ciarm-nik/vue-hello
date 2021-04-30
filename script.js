8b50cff853d7bb51a0412ed4f1dc9169533f337f
Ciarm-nik <nikcia@libero.it>
2021-04-30 09:47:41 +0200

    Aggiunta img

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-// Bonus:
-// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-
-
 
 const app = new Vue({
     el: "#app",
     data: {
-        message: "Hello Vue!"
+        imgUrl: "https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg",
+        message: "Hello Vue!",
     }
 })
+
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
